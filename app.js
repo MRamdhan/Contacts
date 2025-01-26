74e8095c9dd28efc28d09aae31cb78da73b6839f
dani <daniiidani883@gmail.com>
2025-01-26 11:54:03 +0700

    update system

--- app.js
+++ app.js
@@ -1,44 +1,11 @@
-const { rejects } = require('assert');
-const fs = require('fs');
-const { resolve } = require('path');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-const dirPath = './data';
-if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath);
-}
-
-const dataPath = './data/contacts.json';
-if (!fs.existsSync(dataPath)) {
-    fs.writeFileSync(dataPath, '[]', 'utf-8');
-}
-
-const tulusPertanyaan = (pertanyaan) => {
-    return new Promise((resolve, rejects) => {
-        rl.question(pertanyaan, (nama) => {
-            resolve(nama);
-        })
-    })
-}
+const contact = require('./contacts');
 
 const main = async() => {
-    const nama = await tulusPertanyaan('Masukan nama anda : ');
-    const email = await tulusPertanyaan('Masukan email anda : ');
-    const noHp = await tulusPertanyaan('Masukan noHp anda : ');
+    const nama = await contact.tulisPertanyaan('Masukan nama anda : ');
+    const email = await contact.tulisPertanyaan('Masukan email anda : ');
+    const noHp = await contact.tulisPertanyaan('Masukan noHp anda : ');
 
-    const contact = { nama, email, noHp }
-    const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
-    const contacts = JSON.parse(fileBuffer);
-
-    contacts.push(contact);
-
-    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
-    console.log('Data berhasil disimpan');
+    contact.simpanContact(nama, email, noHp);
 }
 
 main();
